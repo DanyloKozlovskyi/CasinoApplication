aa44774de19cea3b49f92cc38b78858c28f13d9a
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-12-12 18:42:31 +0200

    create dataaccess

---
+++ Casino.DataAccess/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.DataAccess.Models
+{
+	public class User
+	{
+		[Key]
+		public Guid Id { get; set; }
+		[Required(ErrorMessage = "First Name can't be blank")]
+		public string FirstName { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Last Name can't be blank")]
+		public string LastName { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Email can't be blank")]
+		[EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
+		public string Email { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Phone Number can't be blank")]
+		public string PhoneNumber { get; set; } = string.Empty;
+	}
+}
